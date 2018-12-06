470518814c8f4f1eb9f8071e433d3f1c39a5cc14
Darren Rambaud <xyzst@users.noreply.github.com>
2018-12-05 23:48:26 -0600

    Added backend components -- yoga graphql server, added basic db connectivity components

---
+++ core/src/db.js
@@ -0,0 +1,12 @@
+// Connects to the remote Prisma DB and allows for querying via JS
+
+const { Prisma } = require('prisma-binding'); // There is no import in node.js?
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: true
+});
+
+module.exports = db;
--- core/src/index.js
+++ core/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middleware to handle cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTENT_URL
+  }
+}, details => {
+  console.log(`Server is now running on port http://localhost:${details.port}`);
+});
