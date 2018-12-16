ca363541cf9feb56fb22d33657ff2dbbc936bf84
Darren Rambaud <xyzst@users.noreply.github.com>
2018-12-15 18:34:43 -0600

    Added ability to upload images to a cloud service -- Cloudinary

--- ui/components/CreateItem.js
+++ ui/components/CreateItem.js
@@ -41,6 +41,25 @@ class CreateItem extends Component {
     this.setState({[name]: val}); // Computed property names (ES6 feat)
   };
 
+  uploadFile = async (e) => {
+    const files = e.target.files;
+    const data = new FormData(); // FormData API (ES6)
+    data.append('file', files[0]);
+    data.append('upload_preset', 'foo3hldm'); // Cloudinary upload preset
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dh46flsyc/image/upload', {
+      method: 'POST',
+      body: data
+    });
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -59,6 +78,17 @@ class CreateItem extends Component {
           }}>
             <Error error={error}/>
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input type="file"
+                       id="file"
+                       name="file"
+                       placeholder="Upload an image"
+                       required
+                       onChange={this.uploadFile}
+                />
+                {this.state.image && (<img src={this.state.image} width="200" alt="Upload preview" />)}
+              </label>
               <label htmlFor="title">
                 Title
                 <input type="text"
